dbe00958d148aad5ec638b7c67155bd09d8f82f6
pvdo <fire_ra@bk.ru>
2026-03-03 17:43:58 +0300

    logging

--- cmd/ssotu/main.go
+++ cmd/ssotu/main.go
@@ -2,18 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+	 slog.String("env", cfg.Env),
+	slog.Any("cfg", cfg),
+	slog.Int("port", cfg.GRPC.Port),
+	)
+
+	log.Debug("debug message")
+
+	log.Error("error message")
+
+
 
 	// TODO: init logger
 
 	// TODO: init app
 
 	// TODO: run gRPC app server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env{
+		case envLocal:
+			log = slog.New(
+				slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+			)
+		case envDev:
+			log = slog.New(
+				slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+			)
+		case envProd:
+			log = slog.New(
+				slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+			)
+	}
+
+	return log
 }
