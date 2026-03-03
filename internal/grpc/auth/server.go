f30d4db7d64a42626e5157b8403a6cd87cc97dc1
pvdo <fire_ra@bk.ru>
2026-03-03 18:13:42 +0300

    stubs auth

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -21,3 +21,17 @@ func (s *serverAPI) Login(
 ) (*ssov1.LoginResponse, error) {
 	panic("implement me")
 }
+
+func (s *serverAPI) Register(
+	ctx context.Context,
+	req *ssov1.RegisterRequest,
+)(*ssov1.RegisterResponse, error){
+	panic("implement me")
+}
+
+func (s *serverAPI) IsAdmin(
+	ctx context.Context,
+	req *ssov1.IsAdminRequest,
+)(*ssov1.IsAdminResponse, error){
+	panic("implement me")
+}
