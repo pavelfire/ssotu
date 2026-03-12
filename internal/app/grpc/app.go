fe2dca968cf12c50eaaa3876f2f51b24e0287263
pvdo <fire_ra@bk.ru>
2026-03-12 18:02:43 +0300

    Must run writed

--- internal/app/grpc/app.go
+++ internal/app/grpc/app.go
@@ -33,6 +33,13 @@ func New(
 	}
 }
 
+// MustRun runs the gRPC server and panics if it fails
+func (a *App) MustRun() {
+	if err := a.Run(); err != nil {
+		panic(err)
+	}
+}
+
 func (a *App) Run() error {
 	const op = "grppcpp.Run"
 	log := a.log.With(
@@ -40,11 +47,26 @@ func (a *App) Run() error {
 		slog.Int("port", a.port),
 	)
 
-	log.Info("starting gRPC server")
-
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
-	if err != nil{
+	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
-	
+
+	log.Info("gRPC server is running", slog.String("addr", l.Addr().String()))
+
+	if err := a.gRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+// Stop stops the gRPC server
+func (a *App) Stop() {
+	const op = "grpcapp.Stop"
+
+	a.log.With(slog.String("op", op)).
+		Info("stopping gRPC server", slog.Int("port", a.port))
+
+	a.gRPCServer.GracefulStop()
 }
