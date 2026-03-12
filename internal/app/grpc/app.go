17b53483925c7cd967c6ccb612c24eef4c3d5384
pvdo <fire_ra@bk.ru>
2026-03-12 17:48:07 +0300

    progress

--- internal/app/grpc/app.go
+++ internal/app/grpc/app.go
@@ -1,9 +1,13 @@
 package grpcapp
 
 import (
+	"fmt"
 	"log/slog"
+	"net"
+
 	// "strconv"
 	authgrpc "ssotu/internal/grpc/auth"
+
 	"google.golang.org/grpc"
 )
 
@@ -27,4 +31,20 @@ func New(
 		gRPCServer: gRPCServer,
 		port:       port, //":" + strconv.Itoa(port),
 	}
+}
+
+func (a *App) Run() error {
+	const op = "grppcpp.Run"
+	log := a.log.With(
+		slog.String("op", op),
+		slog.Int("port", a.port),
+	)
+
+	log.Info("starting gRPC server")
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil{
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	
 }
