911d7acee206e69b1ad1d73c849e4dbfd2d77cdb
pvdo <fire_ra@bk.ru>
2026-03-12 15:23:16 +0300

    grpc app progress

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,2 +1,30 @@
-package app
+package appapp
 
+import (
+	"log/slog"
+	grpcapp "sso/internal/app/grpc"
+
+	"time"
+)
+
+type App struct{
+	log *slog.Logger
+	GRPCSrv *grpcapp.App
+	port int
+}
+
+func New(
+	log *slog.Logger,
+	grpcPort int,
+	storagePath string,
+	tokenTTL time.Duration,
+) *App{
+
+	grpcApp := grpcapp.New(log, grpcPort)
+	
+	// authgrpc.Register(grpcServer)
+
+	return &App{
+		GRPCSrv: grpcApp,
+	}
+}
--- internal/app/grpc/app.go
+++ internal/app/grpc/app.go
@@ -1,5 +1,4 @@
-package grpc
-
+package grpcapp
 
 import (
 	"log/slog"
