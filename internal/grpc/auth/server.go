35190ea274167f5ff6ee56d813701abc4c34089c
pvdo <fire_ra@bk.ru>
2026-03-13 10:36:28 +0300

    handler login ready

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -2,25 +2,66 @@ package auth
 
 import (
 	"context"
+	"go/token"
 
 	ssov1 "github.com/pavelfire/protostu/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appId int,
+	) (token string, err error)
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userId int64, err error)
+	IsAdmin(
+		ctx context.Context,
+		userId int64,
+	) (isAdmin bool, err error)
+}
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
+
+const (
+	emptyValue = 0
+)
 
 func (s *serverAPI) Login(
 	ctx context.Context,
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
+	if req.GetEmail() == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+	if req.GetPassword() == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+	if req.GetAppId() == emptyValue {
+		return nil, status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "failed to login")
+	}
+
 	return &ssov1.LoginResponse{
-		Token: req.GetEmail() + " token89888",
+		Token: token,
 	}, nil
 }
 
