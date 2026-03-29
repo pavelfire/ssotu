9bddb67d07372c52acfe510faaa57b5f5ae1f504
pvdo <fire_ra@bk.ru>
2026-03-29 22:44:20 +0300

    config updated

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,14 +9,35 @@ import (
 )
 
 type Config struct {
-	Env string `yaml:"env" env-default:"local"`
-	StoragePath string `yaml:"storage_path" env-required:"./data"`
-	TokenTTL time.Duration `yaml:"token_ttl" env-default:"1h"`
-	GRPC GRPCCongig `yaml:"grpc"`
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"./data"`
+	TokenTTL    time.Duration `yaml:"token_ttl" env-default:"1h"`
+	GRPC        GRPCCongig    `yaml:"grpc"`
+	HTTPServer  HTTPConfig    `yaml:"http_server"`
+	Clients     ClientConfig  `yaml:"clients"`
+	AppSecret   string        `yaml:"app_secret" env-required:"true" env:"APP_SECRET"`
+}
+
+type HTTPConfig struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+	User        string        `yaml:"user" env-required:"true"`
+	Password    string        `yaml:"password" env-required:"true" env:"HTTP_SERVER_PASSWORD"`
+}
+
+type Client struct {
+	Address      string        `yaml:"address"`
+	Timeout      time.Duration `yaml:"timeout"`
+	RetriesCount int           `yaml:"retriesCount"`
+}
+
+type ClientConfig struct {
+	SSO Client `yaml:"sso"`
 }
 
 type GRPCCongig struct {
-	Port int `yaml:"port"`
+	Port    int           `yaml:"port"`
 	Timeout time.Duration `yaml:"timeout"`
 }
 
@@ -31,7 +52,7 @@ func MustLoad() *Config {
 
 func MustLoadByPath(path string) *Config {
 
-	if _, err := os.Stat(path); os.IsNotExist(err){
+	if _, err := os.Stat(path); os.IsNotExist(err) {
 		panic("config file does not exist: " + path)
 	}
 
@@ -44,12 +65,12 @@ func MustLoadByPath(path string) *Config {
 	return &cfg
 }
 
-//fetchConfigPath fetches config path from comand line flag or environment variable
-//Priority: flag > env > default
-//Default value is empty string
+// fetchConfigPath fetches config path from comand line flag or environment variable
+// Priority: flag > env > default
+// Default value is empty string
 func fetchConfigPath() string {
 	var res string
-	
+
 	// --config="path/to/config.yaml"
 	flag.StringVar(&res, "config", "", "path to config file")
 	flag.Parse()
